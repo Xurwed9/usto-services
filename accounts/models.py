81323dfd100d710f0dc45c5de8b41d90cd462932
Xurshed <xursheddddddd@gmail.com>
2026-06-02 21:31:03 +0500

    add User model

--- accounts/models.py
+++ accounts/models.py
@@ -2,3 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    role_choice=[
+        ('client', 'client'),
+        ('master', 'master')
+    ]
+    email = models.EmailField(unique=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    role = models.CharField(max_length=10, choices=role_choice, default='client')
+    phone = models.CharField(max_length=18, blank=True)
+
+    def __str__(self):
+        return f"{self.username}--{self.phone if self.phone else 'No Phone'}"
