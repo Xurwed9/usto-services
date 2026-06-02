2a3be5b901b2ee065c841b20bb8ea216ae2cc748
Xurshed <xursheddddddd@gmail.com>
2026-06-03 00:06:08 +0500

    add category list in views.py

--- services/views.py
+++ services/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from .models import Category
 
 # Create your views here.
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'services/category_list.html', {'categories': categories})
