00e98723edfb159fa350b9caec9b708f54d6e8f4
Xurshed <xursheddddddd@gmail.com>
2026-06-02 23:41:23 +0500

    add services model

--- services/models.py
+++ services/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-from django.conf import settings
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
 
 class Category(models.Model):
     name = models.CharField(max_length=100, verbose_name="Category Name")
@@ -10,3 +11,31 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+    
+
+class Service(models.Model):
+    master = models.ForeignKey(
+        User, 
+        on_delete=models.CASCADE, 
+        related_name='services',
+        verbose_name="Master / Provider"
+    )
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL, 
+        null=True, 
+        related_name='services',
+        verbose_name="Category"
+    )
+    title = models.CharField(max_length=255, verbose_name="Service Title")
+    description = models.TextField(verbose_name="Detailed Description")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Starting Price (TJS)")
+    image = models.ImageField(upload_to='services/images/', blank=True, null=True, verbose_name="Service Image")
+    
+    is_active = models.BooleanField(default=True, verbose_name="Is Active")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Created At")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Updated At")
+
+
+    def __str__(self):
+        return f"{self.title} | {self.master.username}"
