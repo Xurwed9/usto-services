8fa41c6391f36348cf8949f90c85f756b248dbaf
Xurshed <xursheddddddd@gmail.com>
2026-06-02 21:34:10 +0500

    add Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,17 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username}--{self.phone if self.phone else 'No Phone'}"
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    photo = models.ImageField(upload_to='users/', blank=True, null=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    
+    bio = models.TextField(blank=True, null=True, verbose_name="About Master")
+    experience = models.PositiveIntegerField(default=0, verbose_name="Experience in years")
+    rating = models.DecimalField(max_digits=3, decimal_places=2, default=5.0, verbose_name="Rating")
+
+    def __str__(self):
+        return f"Profile of {self.user.username}"
