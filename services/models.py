bc66752a123d9c298e2b3fde930139c8adfbce12
Xurshed <xursheddddddd@gmail.com>
2026-06-02 23:36:49 +0500

    add category model in services app

--- services/models.py
+++ services/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Category Name")
+    slug = models.SlugField(unique=True, verbose_name="URL Slug")
+    icon = models.ImageField(upload_to='categories/icons/', blank=True, null=True, verbose_name="Category Icon")
+
+    def __str__(self):
+        return self.name
