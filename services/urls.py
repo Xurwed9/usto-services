5d00c9dab34dad237e4a7221b752cc51bd128ae3
Xurshed <xursheddddddd@gmail.com>
2026-06-03 10:48:32 +0500

    add path services-category

--- services/urls.py
+++ services/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.service_list, name='service_list'),
     path('create-services/', views.create_service, name='create_services'),
     path('services/<int:id>/', views.service_detail, name='service_detail'),
+    path('services-category/<slug:slug>/', views.category_services, name='category_services'),
 ]
