69f07d85372db7986ac414ee916d1d4cb998984e
Xurshed <xursheddddddd@gmail.com>
2026-06-03 22:38:34 +0500

    add path password-reset-confirm in urls server

--- server/urls.py
+++ server/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
 
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
     path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
