bde32bbbec01e33890153830fba076e2e63101e2
Xurshed <xursheddddddd@gmail.com>
2026-06-03 11:35:48 +0500

    add path service_search

--- services/urls.py
+++ services/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('service/update/<int:pk>/', views.update_service, name='update_service'),
     path('service/delete/<int:pk>', views.delete_service, name='delete_service'),
     path('service/toggle/<int:pk>/', views.toggle_service_status, name='toggle_service_status'),
+    path('search/', views.service_search, name='service_search'),
 ]
