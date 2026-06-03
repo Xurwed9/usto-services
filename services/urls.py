c9f1265104f5805f55a93dcdffd874f73421dc9b
Xurshed <xursheddddddd@gmail.com>
2026-06-03 07:46:59 +0500

    add path service_list

--- services/urls.py
+++ services/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.category_list, name='category_list'),
+    path('', views.service_list, name='service_list'),
     path('create-services/', views.create_service, name='create_services'),
 ]
