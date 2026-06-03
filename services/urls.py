4ab9e092f2fd12f35ba9c251d7e7cd56f24d405d
Xurshed <xursheddddddd@gmail.com>
2026-06-04 00:07:39 +0500

    add ai_help

--- services/urls.py
+++ services/urls.py
@@ -17,5 +17,4 @@ urlpatterns = [
     path('order-status/<int:pk>/<str:status>/', views.update_order_status, name='update_order_status'),
 
     path('order/<int:order_id>/review/', views.leave_review, name='leave_review'),
-
 ]
