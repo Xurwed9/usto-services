8557333e25b62116c22897dcc33dcd39d91543f1
Xurshed <xursheddddddd@gmail.com>
2026-06-03 07:56:57 +0500

    add path service detail

--- services/urls.py
+++ services/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.service_list, name='service_list'),
     path('create-services/', views.create_service, name='create_services'),
+    path('services/<int:id>/', views.service_detail, name='service_detail'),
 ]
