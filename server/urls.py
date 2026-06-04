9f0de9ab5ec5807decec02755bf9e996cc83c0d4
Xurshed <xursheddddddd@gmail.com>
2026-06-04 12:38:43 +0500

    import i18n_patterns

--- server/urls.py
+++ server/urls.py
@@ -19,9 +19,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('services.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
